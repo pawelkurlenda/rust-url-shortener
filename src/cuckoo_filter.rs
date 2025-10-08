70e33cfdb01875cb4a3798e661c56d394846e555
kurlendapawel <kurlendapawel@gmail.com>
2025-10-08 20:06:25 +0200

    cuckoo_filter start model

--- src/cuckoo_filter.rs
+++ src/cuckoo_filter.rs
@@ -1,1 +1,17 @@
-pub struct CuckooFilter {}
+pub struct CuckooFilter {
+    hasher: DefaultHasher,
+    array_size: u64,
+    bucket_1: Vec<usize>,
+    bucket_2: Vec<usize>,
+}
+
+impl CuckooFilter {
+    pub fn new(size: u64) -> Self {
+        Self {
+            hasher: DefaultHasher::new(),
+            array_size: size,
+            bucket_1: vec![0; size as usize],
+            bucket_2: vec![0; size as usize],
+        }
+    }
+}

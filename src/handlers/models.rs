f5463d1ec0c176fa3c65d6857b7f73be9a8c3fdf
kurlendapawel <kurlendapawel@gmail.com>
2025-10-18 18:29:51 +0200

    seperate models + init model validation

--- src/handlers/models.rs
+++ src/handlers/models.rs
@@ -1,13 +1,33 @@
 use chrono::DateTime;
 use serde::{Deserialize, Serialize};
-use validator::Validate;
+use validator::{Validate, ValidationError};
+
+fn validate_custom_alias(alias: &str) -> Result<(), ValidationError> {
+    Ok(())
+    // let good = (3..=64).contains(&alias.len())
+    //     && alias
+    //         .chars()
+    //         .all(|c| c.is_ascii_alphanumeric() || c == '-' || c == '_');
+    // if good {
+    //     Ok(())
+    // } else {
+    //     Err(ValidationError::new("bad_alias"))
+    // }
+}
+
+fn validate_expiration_date(date: &DateTime<chrono::Utc>) -> Result<(), ValidationError> {
+    Ok(())
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize, Validate)]
 pub struct ShortenRequest {
+    #[validate(url)]
     pub url: String,
     #[serde(default)]
+    #[validate(custom(function = "validate_custom_alias"))]
     pub custom_alias: Option<String>,
     #[serde(default)]
+    #[validate(custom(function = "validate_expiration_date"))]
     pub expires_at: Option<DateTime<chrono::Utc>>,
 }
 
--- src/models.rs
+++ src/models.rs
@@ -2,21 +2,6 @@ use chrono::DateTime;
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 
-#[derive(Debug, Clone, Serialize, Deserialize, Validate)]
-pub struct ShortenRequest {
-    pub url: String,
-    #[serde(default)]
-    pub custom_alias: Option<String>,
-    #[serde(default)]
-    pub expires_at: Option<DateTime<chrono::Utc>>,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ShortenResponse {
-    pub id: String,
-    pub short_url: String,
-}
-
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct LinkRecord {
     pub id: String,

083060e2af5ac175f12292d1089fc0d3ebeee29a
kurlendapawel <kurlendapawel@gmail.com>
2025-10-23 20:57:55 +0200

    some stucts to b+tree

--- src/store/b_plus_tree.rs
+++ src/store/b_plus_tree.rs
@@ -1,7 +1,18 @@
-pub struct BPlusTree {}
+pub struct BPlusTree<K: Ord + Clone, V, const B: usize> {
+    root: Node<K, V, B>,
+}
 
-impl BPlusTree {
-    pub fn new() -> Self {
-        BPlusTree {}
-    }
+enum Node<K: Ord + Clone, V, const B: usize> {
+    Internal(Internal<K, V, B>),
+    Leaf(Leaf<K, V, B>),
+}
+
+struct Internal<K: Ord + Clone, V, const B: usize> {
+    keys: Vec<K>,.
+    children: Vec<Box<Node<K, V, B>>>,
+}
+
+struct Leaf<K: Ord + Clone, V, const B: usize> {
+    keys: Vec<K>,
+    values: Vec<V>,
 }

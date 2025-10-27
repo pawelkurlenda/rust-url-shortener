59b63cbc45fb8a0d3023e5248ca374781323feff
kurlendapawel <kurlendapawel@gmail.com>
2025-10-27 20:14:18 +0100

    add BPlusTreeStore new open file implementation

--- src/store/b_plus_tree.rs
+++ src/store/b_plus_tree.rs
@@ -3,6 +3,7 @@ use std::{collections::HashMap, hash::Hash, path::{Path, PathBuf}, sync::{Arc, M
 use serde::{Deserialize, Serialize};
 use tokio::{fs::File, sync::RwLock};
 
+const WAL_FILE: &str = "wal.log";
 type NodeId = u64;
 
 pub struct BPlusTreeStore {
@@ -10,6 +11,29 @@ pub struct BPlusTreeStore {
     wal: Mutex<File>,
     last_id: Arc<RwLock<u64>>,
     tree: RwLock<BPlusTree>,
+}
+
+impl BPlusTreeStore {
+    pub async fn new(dir: impl AsRef<Path>) -> tokio::io::Result<Self> {
+        let dir = dir.as_ref().to_path_buf();
+        tokio::fs::create_dir_all(&dir).await?;
+        let wal_path = dir.join(WAL_FILE);
+        let wal = File::options()
+            .create(true)
+            .append(true)
+            .open(wal_path)
+            .await?;
+        let tree = BPlusTree {
+            root: 0,
+            nodes: HashMap::new(),
+        };
+        Ok(Self {
+            dir,
+            wal: Mutex::new(wal),
+            last_id: Arc::new(RwLock::new(0)),
+            tree: RwLock::new(tree),
+        })
+    }
 }
 
 pub struct BPlusTree {

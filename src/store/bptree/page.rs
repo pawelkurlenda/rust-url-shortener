0c546c21bdb11b62d5fc8911c7c80105ec28e1ed
kurlendapawel <kurlendapawel@gmail.com>
2025-11-08 20:18:42 +0100

    add code to page

--- src/store/bptree/page.rs
+++ src/store/bptree/page.rs
@@ -38,4 +38,36 @@ impl Page {
             PageKind::Leaf => 2,
         };
     }
+
+    pub fn set_magic(&mut self) {
+        self.buf[0..4].copy_from_slice(&MAGIC.to_le_bytes());
+    }
+
+    pub fn ok_magic(&self) -> bool {
+        u32::from_le_bytes(self.buf[0..4].try_into().unwrap()) == MAGIC
+    }
+
+    pub fn payload_len(&self) -> usize {
+        u32::from_le_bytes(self.buf[12..16].try_into().unwrap()) as usize
+    }
+
+    pub fn set_payload_len(&mut self, n: usize) {
+        self.buf[12..16].copy_from_slice(&(n as u32).to_le_bytes());
+    }
+
+    pub fn lsn(&self) -> u64 {
+        u64::from_le_bytes(self.buf[16..24].try_into().unwrap())
+    }
+
+    pub fn set_lsn(&mut self, n: u64) {
+        self.buf[16..24].copy_from_slice(&n.to_le_bytes());
+    }
+
+    pub fn payload_mut(&mut self) -> &mut [u8] {
+        &mut self.buf[24..]
+    }
+
+    pub fn payload(&self) -> &[u8] {
+        &self.buf[24..]
+    }
 }

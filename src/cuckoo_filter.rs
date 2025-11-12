18e2c05a6b4c3efd42f4b3a92c9eca4d03c0f34b
kurlendapawel <kurlendapawel@gmail.com>
2025-11-12 20:23:17 +0100

    commit

--- src/cuckoo_filter.rs
+++ src/cuckoo_filter.rs
@@ -1,16 +1,26 @@
+use std::collections::hash_map::RandomState as BuildHasher;
 use std::hash::DefaultHasher;
+use std::sync::{Arc, RwLock};
 
-pub struct CuckooFilter {
-    hasher: DefaultHasher,
+pub struct CuckooFilter<const B: usize> {
+    hasher: BuildHasher,
     array_size: u64,
     bucket_1: Vec<usize>,
     bucket_2: Vec<usize>,
+    build: BuildHasher,
+    //buckets: RwLock<Vec<[u16]>>,
+    buckets: RwLock<Vec<[u16; B]>>,
 }
 
-impl CuckooFilter {
-    pub fn new(size: u64) -> Self {
+impl<const B: usize> CuckooFilter<B> {
+    pub fn new(size: usize) -> Self {
+        assert!(size.is_power_of_two(), "size must be power of two");
+
+        let buckets = vec![[0u16; B]; size];
+
         Self {
-            hasher: DefaultHasher::new(),
+            hasher: BuildHasher::default(),
+            build: BuildHasher::default(),
             array_size: size,
             bucket_1: vec![0; size as usize],
             bucket_2: vec![0; size as usize],

512f9802548bd6605652e40b59dec4093cbba3a8
kurlendapawel <kurlendapawel@gmail.com>
2025-11-18 19:10:13 +0100

    commit

--- src/app_settings.rs
+++ src/app_settings.rs
@@ -1,18 +1,18 @@
-use std::sync::OnceLock;
-
 use serde::Deserialize;
+use validator::Validate;
 
-#[derive(Debug, Clone, Deserialize)]
+#[derive(Debug, Clone, Deserialize, Validate)]
 pub struct AppSettings {
-    //#[serde(default = "default_port")]
+    #[validate(range(min = 0, max = 9999))]
     pub port: u16,
-    //#[serde(default = "default_max_shortened_len")]
+    #[validate(range(min = 1, max = 7))]
     pub max_shortened_len: usize,
+    pub store_dir: String,
 }
 
 //static APP_SETTINGS: OnceLock<AppSettings> = OnceLock::new();
 
-pub fn resolve_settings() -> Result<(), config::ConfigError> {
+pub fn resolve_settings() -> Result<(), Box<dyn std::error::Error>> {
     let builder = config::Config::builder()
         .add_source(config::File::with_name("AppSettings"))
         .build()?;
@@ -23,8 +23,9 @@ pub fn resolve_settings() -> Result<(), config::ConfigError> {
     //         builder.add_source(File::with_name(&format!("appsettings.{env}")).required(false));
     // }
 
-    let settings = builder.try_deserialize::<AppSettings>().unwrap();
-    //APP_SETTINGS.set(settings).unwrap();
+    let settings = builder.try_deserialize::<AppSettings>()?;
+
+    settings.validate()?;
 
     settings::init(settings);
     Ok(())
